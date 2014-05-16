3923509665df90f03f962dd2e834dd3917362d15
kasakui <wayne7ho@gmail.com>
2014-05-16 15:34:19 +0800

    Initial Commit

---
+++ SimpleWeather/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  SimpleWeather
+//
+//  Created by Kasakui on 14-5-16.
+//  Copyright (c) 2014年 Kasakui. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
